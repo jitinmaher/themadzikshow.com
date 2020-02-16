b446748eca00ad1d17849f570f37576f72ba4ddc
Jitin <jitin_maherchandani@intuit.com>
2020-02-16 19:42:23 +0530

    chore: update a

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Chandrankit Sharma.", "Mentalist.", "Magician.", "Speaker."],
+      strings: ["Chandrankit Sharma.", "A Mentalist.", "A Magician.", "A Speaker."],
       typeSpeed: 100,
       loop: true,
     });
